15bcc9a61f86d395d8379df01bbf2865d08b330b
pu-lab <pies35895@gmail.com>
2021-03-01 12:02:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#zad1
+napis1, napis2, liczba1, liczba2, ulamek1, ulamek2, zespola1, zespolona2 = "wacek","placek",3, 4, 3.4, 4.6, 3+2j, 4+2j;
+
+print(napis1,napis2,liczba1,liczba2,ulamek1,ulamek2,zespola1,zespolona2)
+
+#zad2
+a = 2
+b = 4
+dzielenie = a/b
+print(dzielenie)
+mnozenie = a*b
+print(mnozenie)
+dodawanie = a+b
+print(dodawanie)
+odejmowanie = a-b
+print(odejmowanie)
+potega = a**b
+print(potega)
+
+#zad3
+
+#zad4
+import math
+print(math.exp(10))
+
+dzialanie = pow(math.log10(5+pow(math.sin(8), 2)*8), 1.0/6)
+print(dzialanie)
+
+#zad5
+imie = "MICHAL"
+nazwisko = "STOLARCZYK"
+print(imie.capitalize(), nazwisko.capitalize())
+
+#zad6
+tekst = "Baila en la calle de noche (yeah) Baila en la calle de día (let's go) Baila en la calle de noche Baila en la calle de día"
+
+print(tekst.count('la'))
+#zad7
+nazwa = "subaru"
+print(nazwa[1], nazwa[5])
+
+#zad8
+print(tekst.split())
